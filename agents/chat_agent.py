297f366b8997366bcfe5f16d0060d28ef45a33a8
bronn2120 <vince@smarthometheaters.com>
2025-05-04 15:37:32 -0400

    Use GOOGLE_CREDENTIALS environment variable

--- agents/chat_agent.py
+++ agents/chat_agent.py
@@ -1,7 +1,8 @@
 import sys
+
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
+import json
 import logging
 import asyncio
 from google.cloud import speech, vision
